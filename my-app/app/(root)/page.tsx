cac40d78c5e4af5c93ee0345cb46e17849caf4fa
iamshmidt <yuliia.shmidt@gmail.com>
2023-09-01 22:14:52 -0400

    Modal

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -4,14 +4,24 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { UserButton } from "@clerk/nextjs";
 import { Modal } from '@/components/ui/modal'
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
 const SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if(!isOpen){
+            onOpen();
+        }
+    }, [isOpen,onOpen]);
     return (
         <div className='p-4'>
-            this is a protected route
-            <Modal title="Modal title" description="Modal description" isOpen={true} onClose={() => { }}>
+            Root Page 
+            {/* <Modal title="Modal title" description="Modal description" isOpen={true} onClose={() => { }}>
                 Children
             </Modal>
-            <UserButton afterSignOutUrl="/" />
+            <UserButton afterSignOutUrl="/" /> */}
         </div>
     )
 }

02837ab2216fd4448e499dd8bc05236ee8ac8ca6
iamshmidt <yuliia.shmidt@gmail.com>
2023-09-14 20:40:37 -0400

    navbar wip

---
+++ my-app/components/store-switcher.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { PopoverTrigger } from "@/components/ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Store } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>;
+
+interface StoreSwitcherProps  extends PopoverTriggerProps{
+    items:Store[]
+}
+
+function StoreSwitcher({
+    className,
+    items = []
+}: StoreSwitcherProps) {
+    const storeModal = useStoreModal();
+    const params = useParams();
+    const router = useRouter();
+
+    const formattedItems = items.map((item) => ({
+        label: item.name,
+        value: item.id
+    }));
+
+    const currentStore = formattedItems.find((item) => item.value === params.storeId);
+
+    return ( 
+        <div>
+            Store Switcher
+        </div>
+    );
+}
+
+export default StoreSwitcher;

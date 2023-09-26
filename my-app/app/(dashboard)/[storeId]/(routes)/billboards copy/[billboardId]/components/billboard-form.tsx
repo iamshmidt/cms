27ec9f12951fa9919587af4596eac71f2ef8c8c4
iamshmidt <yuliia.shmidt@gmail.com>
2023-09-25 22:41:46 -0400

    products wip

---
+++ my-app/app/(dashboard)/[storeId]/(routes)/billboards copy/[billboardId]/components/billboard-form.tsx
@@ -0,0 +1,120 @@
+
+"use client"
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Billboard } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useState } from "react";
+import { set, useForm } from "react-hook-form";
+import { z } from "zod";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import AlertModal from "@/components/modals/alert-modal";
+import ImageUpload from "@/components/ui/image-upload";
+
+const formSchema = z.object({
+    label: z.string().min(1),
+    imageUrl: z.string().min(1),
+});
+
+
+type BillboardFormValues = z.infer<typeof formSchema>;
+
+interface BillboardFormProps {
+    initialData: Billboard | null;
+}
+
+export const BillboardForm: React.FC<BillboardFormProps> = ({
+    initialData,
+}) => {
+    const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    const title = initialData ? "Edit billboard" : "New billboard";
+    const description = initialData ? "Edit billboard" : "Add a new billboard";
+    const toastMessage = initialData ? "Billboard updated" : "Billboard created";
+    const action = initialData ? "Save changes" : "Create";
+
+    const params = useParams();
+    const router = useRouter();
+    const form = useForm<BillboardFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData || {
+            label: '',
+            imageUrl: ''
+        },
+    });
+
+    const onSubmit = async (data: BillboardFormValues) => {
+        try {
+            setLoading(true)
+            if(initialData){
+                await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data)
+            } else {
+                await axios.post(`/api/${params.storeId}/billboards`, data)
+            }
+            router.refresh();
+            router.push(`/${params.storeId}/billboards`)
+            toast.success(toastMessage)
+        } catch (error) {
+            toast.error("Something went wrong")
+        } finally {
+            setLoading(false)
+        }
+    }
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`)
+            router.refresh();
+            router.push(`/${params.storeId}/billboards}`)
+            toast.success("Billboard deleted")
+        } catch (error) {
+            toast.error("Make sureyou removed all categories using this billboard.")
+        } finally {
+            setLoading(false)
+        }
+    }
+    return (
+        <>
+            <AlertModal loading={loading} isOpen={open} onClose={() => setOpen(false)} onConfirm={onDelete}></AlertModal>
+            <div className="flex items-center justify-between">
+                <Heading title={title} description={description}></Heading>
+                {initialData && (
+                    <Button disabled={loading} variant="destructive" size="icon" onClick={() => setOpen(true)}>
+                        <Trash className="h-4 w-4"></Trash>
+                    </Button>
+                )}
+            </div>
+            <Separator></Separator>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full pt-4 pb-4">
+                    <FormField control={form.control} name="imageUrl" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Background Image</FormLabel>
+                            <FormControl>
+                               <ImageUpload value={field.value ? [field.value] : []} disabled={loading} onChange={(url) => field.onChange(url)} onRemove={()=> field.onChange("")}></ImageUpload>
+                            </FormControl>
+                        </FormItem>
+                    )}></FormField>
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField control={form.control} name="label" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Label</FormLabel>
+                                <FormControl>
+                                    <Input disabled={loading} placeholder="Billboard label" {...field}></Input>
+                                </FormControl>
+                            </FormItem>
+                        )}></FormField>
+                    </div>
+                    <Button disabled={loading} type="submit">{action}</Button>
+                </form>
+            </Form>
+        </>
+    )
+}
---
+++ my-app/app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,47 @@
+import prismadb from "@/lib/prismadb";
+import { ProductClient } from "./components/client";
+import { ProductColumn } from "./components/columns";
+import {format} from 'date-fns';
+import { formatter } from "@/lib/utils";
+
+const ProductsPage = async ({
+    params,
+}: { params: { storeId: string } }) => {
+    const products = await prismadb.product.findMany({
+        where: {
+            storeId: params.storeId,
+        },
+        include: {
+            category:true,
+            size:true,
+            color:true,
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    const formattedProduct: ProductColumn[] = products.map((item) => ({
+        id: item.id,
+        name: item.name,
+        isFeatured: item.isFeatured,
+        isArchived: item.isArchived,
+        price: formatter.format(item.price.toNumber()),
+        category: item.category.name,
+        size: item.category.name,
+        color: item.category.name,
+        createdAt: format(item.createdAt, 'MMMM dd, yyyy')
+    }));
+
+
+    return (
+
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+
+                <ProductClient data={formattedProduct}></ProductClient>
+            </div>
+        </div>
+    );
+}
+export default ProductsPage;

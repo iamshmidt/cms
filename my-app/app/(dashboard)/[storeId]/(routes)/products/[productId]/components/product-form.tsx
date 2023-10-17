f775e2e7a76fdf1c0129a4e5bbcca22bdee65d23
iamshmidt <yuliia.shmidt@gmail.com>
2023-10-16 20:45:33 -0400

    Discount set up

--- my-app/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ my-app/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -8,7 +8,7 @@ import { Separator } from "@/components/ui/separator";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Product, Image, Category, Color, Size } from "@prisma/client";
 import { Trash } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { set, useForm } from "react-hook-form";
 import { z } from "zod";
 import toast from "react-hot-toast";
@@ -20,142 +20,181 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Checkbox } from "@/components/ui/checkbox";
 
 const formSchema = z.object({
-    name: z.string().min(1),
-    images: z.object({ url: z.string() }).array(),
-    price: z.coerce.number().min(1),
-    quantity: z.coerce.number().min(0).max(5),
-    categoryId: z.string().min(1),
-    colorId: z.string().min(1),
-    sizeId: z.string().min(1),
-    isFeatured: z.boolean().default(false).optional(),
-    isArchived: z.boolean().default(false).optional()
+  name: z.string().min(1),
+  images: z.object({ url: z.string() }).array(),
+  price: z.coerce.number().min(1),
+  quantity: z.coerce.number().min(0).max(5),
+  categoryId: z.string().min(1),
+  colorId: z.string().min(1),
+  sizeId: z.string().min(1),
+  isFeatured: z.boolean().default(false).optional(),
+  isArchived: z.boolean().default(false).optional(),
+  discount: z.number().min(0).max(100).optional(),
+  priceAfterDiscount: z.coerce.number().min(0).optional(),
 });
 
 type ProductFormValues = z.infer<typeof formSchema>
 
 interface ProductFormProps {
-    initialData: Product & {
-        images: Image[]
-    } | null;
-    categories: Category[];
-    colors: Color[];
-    sizes: Size[];
+  initialData: Product & {
+    images: Image[]
+  } | null;
+  categories: Category[];
+  colors: Color[];
+  sizes: Size[];
+  price: number;
 };
 
 export const ProductForm: React.FC<ProductFormProps> = ({
-    initialData,
-    categories,
-    colors,
-    sizes
+  initialData,
+  categories,
+  colors,
+  sizes,
+  price
 }) => {
-    const [open, setOpen] = useState(false)
-    const [loading, setLoading] = useState(false)
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [newPrice, setNewPrice] = useState(0);
 
-    const title = initialData ? "Edit product" : "New product";
-    const description = initialData ? "Edit product" : "Add a new product";
-    const toastMessage = initialData ? "Product updated" : "Product created";
-    const action = initialData ? "Save changes" : "Create";
+  const title = initialData ? "Edit product" : "New product";
+  const description = initialData ? "Edit product" : "Add a new product";
+  const toastMessage = initialData ? "Product updated" : "Product created";
+  const action = initialData ? "Save changes" : "Create";
 
-    const params = useParams();
-    const router = useRouter();
+  const params = useParams();
+  const router = useRouter();
+  
 
-    const defaultValues = initialData ? {
-        ...initialData,
-        price: parseFloat(String(initialData?.price)),
-    } : {
-        name: '',
-        images: [],
-        price: 0,
-        quantity: 0,
-        categoryId: '',
-        colorId: '',
-        sizeId: '',
-        isFeatured: false,
-        isArchived: false,
+  const defaultValues = initialData ? {
+    ...initialData,
+    price: parseFloat(String(initialData?.price)),
+    priceAfterDiscount: parseFloat(String(initialData?.priceAfterDiscount)),
+  } : {
+    name: '',
+    images: [],
+    price: 0,
+    quantity: 0,
+    categoryId: '',
+    colorId: '',
+    sizeId: '',
+    isFeatured: false,
+    isArchived: false,
+    discount: 0,
+    priceAfterDiscount: 0,
+  }
+
+  const form = useForm<ProductFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues
+  });
+
+  useEffect(() => {
+    console.log('PRICE', initialData?.price)
+    // if (form.getValues("price") && form.getValues("discount")) {
+    //   const price = parseFloat(form.getValues("price"));
+    //   const discount = parseFloat(form.getValues("discount")) / 100;
+    //   const discountedPrice = price * (1 - discount);
+    //   setNewPrice(discountedPrice);
+    // }
+    const discountValue = form.getValues("discount");
+    console.log('DISCOUNT VALUE', discountValue)
+  }, [form.getValues("price"), form.getValues("discount")]);
+
+  console.log('NEW PRICE', newPrice)
+console.log('PRICE', initialData?.price)
+
+const changePrice = (e: any) => {
+  const price = parseFloat(e.target.value);
+  form.setValue("price", price);
+
+  const discountValue = form.getValues("discount");
+  console.log('DISCOUNT VALUE', discountValue)
+  const discount = discountValue ? parseFloat(discountValue) / 100 : 0;
+  const discountedPrice = price * (1 - discount);
+
+  form.setValue("priceAfterDiscount", discountedPrice);
+
+  setNewPrice(discountedPrice);
+}
+
+
+  const onSubmit = async (data: ProductFormValues) => {
+    try {
+      setLoading(true)
+      if (initialData) {
+        await axios.patch(`/api/${params.storeId}/products/${params.productId}`, data)
+      } else {
+        await axios.post(`/api/${params.storeId}/products`, data)
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/products`)
+      toast.success(toastMessage)
+    } catch (error) {
+      toast.error("Something went wrong")
+    } finally {
+      setLoading(false)
     }
-
-    const form = useForm<ProductFormValues>({
-        resolver: zodResolver(formSchema),
-        defaultValues
-    });
-
-    const onSubmit = async (data: ProductFormValues) => {
-        try {
-            setLoading(true)
-            if (initialData) {
-                await axios.patch(`/api/${params.storeId}/products/${params.productId}`, data)
-            } else {
-                await axios.post(`/api/${params.storeId}/products`, data)
-            }
-            router.refresh();
-            router.push(`/${params.storeId}/products`)
-            toast.success(toastMessage)
-        } catch (error) {
-            toast.error("Something went wrong")
-        } finally {
-            setLoading(false)
-        }
+  }
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`/api/${params.storeId}/products/${params.productId}`)
+      router.refresh();
+      router.push(`/${params.storeId}/products}`)
+      toast.success("Product deleted")
+    } catch (error) {
+      toast.error("Make sureyou removed all categories using this product.")
+    } finally {
+      setLoading(false)
     }
-    const onDelete = async () => {
-        try {
-            setLoading(true)
-            await axios.delete(`/api/${params.storeId}/products/${params.productId}`)
-            router.refresh();
-            router.push(`/${params.storeId}/products}`)
-            toast.success("Product deleted")
-        } catch (error) {
-            toast.error("Make sureyou removed all categories using this product.")
-        } finally {
-            setLoading(false)
-        }
-    }
-    return (
-        <>
-            <AlertModal loading={loading} isOpen={open} onClose={() => setOpen(false)} onConfirm={onDelete}></AlertModal>
-            <div className="flex items-center justify-between">
-                <Heading title={title} description={description}></Heading>
-                {initialData && (
-                    <Button disabled={loading} variant="destructive" size="icon" onClick={() => setOpen(true)}>
-                        <Trash className="h-4 w-4"></Trash>
-                    </Button>
-                )}
-            </div>
-            <Separator></Separator>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full pt-4 pb-4">
-                    <FormField
-                        control={form.control}
-                        name="images"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Images</FormLabel>
-                                <FormControl>
-                                    <ImageUpload
-                                        value={field.value.map((image) => image.url)}
-                                        disabled={loading}
-                                        onChange={(url) => field.onChange([...field.value, { url }])}
-                                        onRemove={(url) => field.onChange([...field.value.filter((current) => current.url !== url)])}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <div className="grid grid-cols-3 gap-8">
-                        <FormField
-                            control={form.control}
-                            name="name"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Name</FormLabel>
-                                    <FormControl>
-                                        <Input disabled={loading} placeholder="Product name" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                            <FormField
+  }
+  return (
+    <>
+      <AlertModal loading={loading} isOpen={open} onClose={() => setOpen(false)} onConfirm={onDelete}></AlertModal>
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description}></Heading>
+        {initialData && (
+          <Button disabled={loading} variant="destructive" size="icon" onClick={() => setOpen(true)}>
+            <Trash className="h-4 w-4"></Trash>
+          </Button>
+        )}
+      </div>
+      <Separator></Separator>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full pt-4 pb-4">
+          <FormField
+            control={form.control}
+            name="images"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Images</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value.map((image) => image.url)}
+                    disabled={loading}
+                    onChange={(url) => field.onChange([...field.value, { url }])}
+                    onRemove={(url) => field.onChange([...field.value.filter((current) => current.url !== url)])}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input disabled={loading} placeholder="Product name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* <FormField
               control={form.control}
               name="price"
               render={({ field }) => (
@@ -167,8 +206,21 @@ export const ProductForm: React.FC<ProductFormProps> = ({
                   <FormMessage />
                 </FormItem>
               )}
+            /> */}
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price</FormLabel>
+                  <FormControl>
+                    <Input type="number" disabled={loading} placeholder="9.99" {...field} onChange={(e)=>changePrice(e)}/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-                            <FormField
+            <FormField
               control={form.control}
               name="quantity"
               render={({ field }) => (
@@ -181,7 +233,66 @@ export const ProductForm: React.FC<ProductFormProps> = ({
                 </FormItem>
               )}
             />
+            <FormField
+              control={form.control}
+              name="discount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Discount</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      disabled={loading}
+                      placeholder="0"
+                      {...field}
+                      onChange={(e) => {
+                        const parsedValue = parseFloat(e.target.value);
+                        field.onChange(parsedValue);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
              <FormField
+              control={form.control}
+              name="priceAfterDiscount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price After Discount</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      disabled={loading}
+                      placeholder="0"
+                      {...field}
+                      // onChange={(e) => {
+                      //   const parsedValue = parseFloat(e.target.value);
+                      //   field.onChange(parsedValue);
+                      // }}
+                      value={newPrice}
+                      readOnly
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* <FormItem>
+              <FormLabel>Price After Discount</FormLabel>
+              <FormControl>
+                <Input
+                  type="number"
+                  disabled={loading}
+                  value={isNaN(newPrice) ? "Error" : newPrice.toFixed(2)}
+                  readOnly
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem> */}
+            {/* <div>New Price: {newPrice.toFixed(2)}</div> */}
+            <FormField
               control={form.control}
               name="categoryId"
               render={({ field }) => (
@@ -293,10 +404,10 @@ export const ProductForm: React.FC<ProductFormProps> = ({
                 </FormItem>
               )}
             />
-                    </div>
-                    <Button disabled={loading} type="submit">{action}</Button>
-                </form>
-            </Form>
-        </>
-    )
+          </div>
+          <Button disabled={loading} type="submit">{action}</Button>
+        </form>
+      </Form>
+    </>
+  )
 }

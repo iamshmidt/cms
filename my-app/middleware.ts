3a9a61c299dbb0bc97c99ab7167d9ea3ebb77703
iamshmidt <yuliia.shmidt@gmail.com>
2024-01-15 14:45:32 -0500

    init

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -4,11 +4,14 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"]
+  publicRoutes: ["/","/api/:path*"]
 });
  
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 const checkNums = (num1: number, num2: number): boolean => {
   return num1 > num2;
